83ed37785c5ab1fd56a4cf830e45dea20278c80c
priya <priya011@gmail.com>
2023-06-18 09:09:51 +0530

    getElementByClassName is done

--- script.js
+++ script.js
@@ -3,5 +3,10 @@
 // console.log(heading);
 
 //getElementByTagName
-const heading=document.getElementsByTagName('h1');
-console.log(heading);
+// const heading=document.getElementsByTagName('h1');
+// console.log(heading[1]);
+
+
+//getElementByClassName
+const heading=document.getElementsByClassName('demo');
+console.log(heading[0]);
