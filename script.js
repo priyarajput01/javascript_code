ed07e7044a681e124c7acc0c108920745c138c3c
priya <priya011@gmail.com>
2023-06-18 09:39:52 +0530

    accessing parent node

--- script.js
+++ script.js
@@ -1,20 +1,4 @@
-// getElementById
-// const heading=document.getElementById('heading');
-// console.log(heading);
-
-//getElementByTagName
-// const heading=document.getElementsByTagName('h1');
-// console.log(heading[1]);
-
-
-//getElementByClassName
-// const heading=document.getElementsByClassName('demo');
-// console.log(heading[0]);
-
-//quary selector
-// const heading=document.querySelector('#heading');
-// console.log(heading);
-
-//quarySelectorAll
-const heading=document.querySelectorAll('h1');
-console.log(heading[3]);
+// accessing parent node
+const child=document.querySelector('#child');
+const parent=child.parentNode;
+console.log(parent);
